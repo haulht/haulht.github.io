7d1533d6fee2e90211710e237e66a156cf0e33b7
haulht <haulht91@gmail.com>
2017-07-21 17:05:47 +0700

    [CropOB] Add demo basic and demo crop

--- cropob/js/custom.js
+++ cropob/js/custom.js
@@ -1,5 +1,18 @@
+var cropobBasic;
+var cropobCrop;
 $(document).ready(function() {
-  var cropobBasic = $("#cropob_basic").croima({
+  cropobBasic = $("#cropob_basic").croima({
     'image_url'	:'1.JPG',
   });
+  cropobCrop = $("#cropob_crop").croima({
+    'image_url' :'1.JPG',
+    'type_feature': 2
+  });
 });
+
+function downloadImgCropOB1(){
+  if(!cropobCrop)
+    return null;
+  var data = cropobCrop.croima('getImageData');
+  window.open(data);
+}
